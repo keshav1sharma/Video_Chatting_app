67e1c876680b35ef1f119d6f55f2bd56ee9d20cc
Keshav Sharma <keshavsharma@Keshavs-MacBook-Air-2.local>
2023-12-02 11:44:23 +0530

    resolved CORS error

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,11 +4,11 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://video-chatting-app-backend.vercel.app/' , {
-  withCredentials : true,
-  extraHeaders : {
-    "Access-Control-Allow-Origin": "https://p2p-videochat-app.vercel.app"
-  }
+const socket = io('https://video-chatting-app-backend.vercel.app/',{
+  withCredentials:true,
+  extraHeaders:{
+    "Access-Control-Allow-Origin":'https://p2p-videochat-app.vercel.app',
+  },
 });
 
 const ContextProvider = ({ children }) => {
